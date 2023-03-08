259b23241adfbcec84e3745891afee6b6c2efe73
ingsoft <ingsoft1999@gmail.com>
2023-03-08 20:03:35 +0900

    230308-333

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,6 @@
 import { Knex, knex } from "knex";
 import path from "path";
-import Article from "../models/article";
-import LoginHistoryEntry from "../models/loginHistoryEntry";
+
 import User from "../models/user";
 
 export const db = knex({
@@ -12,52 +11,8 @@ export const db = knex({
 });
 
 export const getUsers = (): Knex.QueryBuilder<User, User[]> => db("users");
-export const getArticles = (): Knex.QueryBuilder<Article, Article[]> =>
-  db("articles");
-export const getLoginHistory = (): Knex.QueryBuilder<
-  LoginHistoryEntry,
-  LoginHistoryEntry[]
-> => db("loginHistory");
+
 
 export const initializeTables = async () => {
-  if (!(await db.schema.hasTable("users"))) {
-    await db.schema.createTable("users", (table) => {
-      table.increments("id", {
-        primaryKey: true,
-      });
-      table.string("username").notNullable();
-      table.string("password").notNullable();
-      table.string("email").notNullable();
-      table.string("firstName").notNullable();
-      table.string("lastName").notNullable();
-      table.integer("age").defaultTo(1);
-      table.string("gender").defaultTo("other");
-      table.integer("points").defaultTo(100);
-      table.integer("leastSwitches").defaultTo(-1);
-      table.boolean("isAdmin").defaultTo(false);
-    });
-  }
-
-  if (!(await db.schema.hasTable("articles"))) {
-    await db.schema.createTable("articles", (table) => {
-      table.increments("id", {
-        primaryKey: true,
-      });
-      table.string("title").notNullable();
-      table.string("content").notNullable();
-      table.string("likedBy").defaultTo("[]"); // JSON.stringify as number[]
-      table.integer("lastUpdated").notNullable();
-    });
-  }
-
-  if (!(await db.schema.hasTable("loginHistory"))) {
-    await db.schema.createTable("loginHistory", (table) => {
-      table.increments("id", {
-        primaryKey: true,
-      });
-      table.string("username").notNullable();
-      table.bigInteger("date").notNullable();
-      table.boolean("successful").notNullable();
-    });
-  }
+  
 };
